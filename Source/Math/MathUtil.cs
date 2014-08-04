7a01ddd80daa80ebd6dc27311b29c90791264e0a
robert <robert>
2014-08-04 17:43:45 -0500

    Added a few math helper functions

---
+++ Source/Math/MathUtil.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Game.Util
+{
+    /// <summary>
+    /// Additional math helper functions beyond what Unity provides.
+    /// </summary>
+    public class MathUtil
+    {
+        /// <summary>
+        /// Snaps the given value to the nearest lower multiple of the step value.
+        /// For example, given step of 5, values in [0, 5) will snap to 0; 
+        /// values in [5, 10) will snap to 5, values in [-5, 0) will snap to -5, etc.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        public static float Quantize(float value, float step)
+        {
+            return Mathf.Floor(value / step) * step;
+        }
+
+        /// <summary>
+        /// Snaps the given value to the nearest lower multiple of the step value.
+        /// For example, given step of 5, integer values in [0, 4] will snap to 0; 
+        /// values in [5, 9] will snap to 5, values in [-5, -1] will snap to -5, etc.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="step"></param>
+        /// <returns></returns>
+        public static int Quantize(int value, int step)
+        {
+            return Mathf.FloorToInt((float) value / step) * step;
+        }
+
+        /// <summary>
+        /// Clamps the value to be within the range [min, max]
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static float Clamp(float value, float min = 0f, float max = 1f)
+        {
+            if (value < min) { return min; }
+            if (value > max) { return max; }
+            return value;
+        }
+
+        /// <summary>
+        /// Clamps the value to be within the range [min, max]
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min) { return min; }
+            if (value > max) { return max; }
+            return value;
+        }
+
+        public static bool IsEven(int value) { return (value & 1) == 0; }
+        public static bool IsEven(uint value) { return (value & 1) == 0; }
+        public static bool IsEven(float value) { return ((int)value & 1) == 0; }
+
+        public static bool IsOdd(int value) { return (value & 1) == 1; }
+        public static bool IsOdd(uint value) { return (value & 1) == 1; }
+        public static bool IsOdd(float value) { return ((int)value & 1) == 1; }
+    }
+}
---
+++ UnitTests/Math/MathUtilTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.Util;
+
+namespace SomaSim.Math
+{
+    [TestClass]
+    public class MathUtilTest
+    {
+        [TestMethod]
+        public void TestQuantize()
+        {
+            // floats
+            Assert.IsTrue(MathUtil.Quantize(-10f, 5f) == -10f);
+            Assert.IsTrue(MathUtil.Quantize(-5.01f, 5f) == -10f);
+            Assert.IsTrue(MathUtil.Quantize(-5f, 5f) == -5f);
+            Assert.IsTrue(MathUtil.Quantize(-0.01f, 5f) == -5f);
+            Assert.IsTrue(MathUtil.Quantize(0f, 5f) == 0f);
+            Assert.IsTrue(MathUtil.Quantize(4.99f, 5f) == 0f);
+            Assert.IsTrue(MathUtil.Quantize(5f, 5f) == 5f);
+            Assert.IsTrue(MathUtil.Quantize(9.99f, 5f) == 5f);
+
+            // ints
+            Assert.IsTrue(MathUtil.Quantize(-10, 5) == -10);
+            Assert.IsTrue(MathUtil.Quantize(-6, 5) == -10);
+            Assert.IsTrue(MathUtil.Quantize(-5, 5) == -5);
+            Assert.IsTrue(MathUtil.Quantize(-1, 5) == -5);
+            Assert.IsTrue(MathUtil.Quantize(0, 5) == 0);
+            Assert.IsTrue(MathUtil.Quantize(4, 5) == 0);
+            Assert.IsTrue(MathUtil.Quantize(5, 5) == 5);
+            Assert.IsTrue(MathUtil.Quantize(9, 5) == 5);
+        }
+
+        [TestMethod]
+        public void TestClamp()
+        {
+            // floats
+            Assert.IsTrue(MathUtil.Clamp(-1f, 0f, 1f) == 0f);
+            Assert.IsTrue(MathUtil.Clamp(0f, 0f, 1f) == 0f);
+            Assert.IsTrue(MathUtil.Clamp(0.5f, 0f, 1f) == 0.5f);
+            Assert.IsTrue(MathUtil.Clamp(1f, 0f, 1f) == 1f);
+            Assert.IsTrue(MathUtil.Clamp(1.1f, 0f, 1f) == 1f);
+
+            // ints
+            Assert.IsTrue(MathUtil.Clamp(-1, 0, 10) == 0);
+            Assert.IsTrue(MathUtil.Clamp(0, 0, 10) == 0);
+            Assert.IsTrue(MathUtil.Clamp(5, 0, 10) == 5);
+            Assert.IsTrue(MathUtil.Clamp(10, 0, 10) == 10);
+            Assert.IsTrue(MathUtil.Clamp(11, 0, 10) == 10);
+        }
+
+        [TestMethod]
+        public void TestEvenOdd()
+        {
+            Assert.IsTrue(MathUtil.IsEven(-2));
+            Assert.IsTrue(MathUtil.IsEven(0));
+            Assert.IsTrue(MathUtil.IsEven((uint)2));
+            Assert.IsTrue(MathUtil.IsEven(2f));
+            Assert.IsTrue(MathUtil.IsEven(2.1f));
+            Assert.IsFalse(MathUtil.IsOdd(-2));
+            Assert.IsFalse(MathUtil.IsOdd(0));
+            Assert.IsFalse(MathUtil.IsOdd((uint)2));
+            Assert.IsFalse(MathUtil.IsOdd(2f));
+            Assert.IsFalse(MathUtil.IsOdd(2.1f));
+
+            Assert.IsTrue(MathUtil.IsOdd(-1));
+            Assert.IsTrue(MathUtil.IsOdd(1));
+            Assert.IsTrue(MathUtil.IsOdd((uint)1));
+            Assert.IsTrue(MathUtil.IsOdd(1f));
+            Assert.IsTrue(MathUtil.IsOdd(1.1f));
+            Assert.IsFalse(MathUtil.IsEven(-1));
+            Assert.IsFalse(MathUtil.IsEven(1));
+            Assert.IsFalse(MathUtil.IsEven((uint)1));
+            Assert.IsFalse(MathUtil.IsEven(1f));
+            Assert.IsFalse(MathUtil.IsEven(1.1f));
+        }
+    }
+}
