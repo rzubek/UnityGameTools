e756b21d874ebfd3a840fdf068949f5f8c48b089
robert <robert>
2014-09-01 22:26:46 -0500

    Added smart stacks: they tell their contained objects about when they're pushed and popped, and when they become topmost or get pushed further down the stack.

---
+++ Source/Collections/SmartStack.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.Collections
+{
+    /// <summary>
+    /// Interface for stack elements that get informed when they're pushed or popped,
+    /// as well as start or end being the topmost element.
+    /// </summary>
+    public interface ISmartStackElement
+    {
+        /// <summary>
+        /// Called after this element was pushed onto the stack. It has not yet been
+        /// activated as the topmost element, that will happen after this call.
+        /// </summary>
+        void OnPushed (object stack);
+
+        /// <summary>
+        /// Called when the stack element becomes the topmost one. This could happen
+        /// either because 1. this element was just pushed on top of the stack, or 
+        /// 2. when this element was right beneath another topmost element, that just got popped.
+        /// </summary>
+        void OnActivated ();
+
+        /// <summary>
+        /// Called when the stack element ceases to be the topmost one. This happens either
+        /// as a result of 1. another element being pushed on top of this one, or
+        /// 2. right before this element is popped off the stack.
+        /// </summary>
+        void OnDeactivated ();
+
+        /// <summary>
+        /// Called just before this topmost stack element is removed from the stack.
+        /// </summary>
+        void OnPopped ();
+    }
+
+    abstract public class AbstractSmartStackElement : ISmartStackElement
+    {
+        public object Stack { get; private set; }
+        public bool IsActive { get; private set; }
+        public bool IsOnStack { get { return Stack != null; } }
+
+        /// <inheritDoc />
+        virtual public void OnPushed (object stack) {
+            this.Stack = stack;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnActivated () {
+            this.IsActive = true;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnDeactivated () {
+            this.IsActive = false;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnPopped () {
+            this.Stack = null;
+        }
+    }
+
+    [DebuggerDisplay("Count = {Count}")]
+    public class SmartStack<T> : IEnumerable<T>, ICollection<T>, IEnumerable
+        where T : class, ISmartStackElement
+    {
+        private Stack<T> _stack;
+
+        public SmartStack () {
+            this._stack = new Stack<T>();
+        }
+
+        /// <summary>
+        /// Returns true if the stack is empty
+        /// </summary>
+        public bool Empty { get { return _stack.Count == 0; } }
+
+        /// <summary>
+        /// Pushes a new element onto the stack and activates it as the topmost one.
+        /// It will also deactivate the previous topmost element, if it exists.
+        /// </summary>
+        /// <param name="element"></param>
+        public void Push (T element) {
+            if (!Empty) {
+                T top = _stack.Peek();
+                top.OnDeactivated();
+            }
+
+            _stack.Push(element);
+            element.OnPushed(this);
+            element.OnActivated();
+        }
+
+        /// <summary>
+        /// Removes and return the topmost element on the stack, or null if empty.
+        /// Popping an element off the stack will activate the next element under it, if it exists.
+        /// </summary>
+        /// <returns></returns>
+        public T Pop () {
+            if (Empty) {
+                return null;
+            }
+
+            T old = _stack.Peek();
+            old.OnDeactivated();
+            old.OnPopped();
+            _stack.Pop();
+
+            if (!Empty) {
+                T top = _stack.Peek();
+                top.OnActivated();
+            }
+
+            return old;
+        }
+
+        /// <summary>
+        /// Returns the topmost element on the stack without removing it, or null if empty.
+        /// </summary>
+        /// <returns></returns>
+        public T Peek () {
+            return Empty ? null : _stack.Peek();
+        }
+
+        /// <summary>
+        /// Returns the topmost element on the stack without removing it, or null if empty.
+        /// </summary>
+        /// <returns></returns>
+        public T Top { get { return Peek(); } }
+
+        #region IEnumerable Members
+
+        /// <inheritDoc />
+        public IEnumerator<T> GetEnumerator () {
+            return _stack.GetEnumerator();
+        }
+
+        /// <inheritDoc />
+        IEnumerator IEnumerable.GetEnumerator () {
+            return _stack.GetEnumerator();
+        }
+
+        #endregion
+
+        #region ICollection Members
+
+        /// <inheritDoc />
+        public void CopyTo (T[] array, int index) {
+            _stack.CopyTo(array, index);
+        }
+
+        /// <inheritDoc />
+        public int Count {
+            get { return _stack.Count; }
+        }
+
+        /// <inheritDoc />
+        public bool IsSynchronized {
+            get { return false; }
+        }
+
+        /// <inheritDoc />
+        public object SyncRoot {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <inheritDoc />
+        public void Add (T item) {
+            Push(item);
+        }
+
+        /// <inheritDoc />
+        public void Clear () {
+            while (Count > 0) {
+                Pop();
+            }
+        }
+
+        /// <inheritDoc />
+        public bool Contains (T item) {
+            return _stack.Contains(item);
+        }
+
+        /// <inheritDoc />
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        /// <inheritDoc />
+        public bool Remove (T item) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/Collections/SmartStackTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SomaSim.Collections
+{
+    [TestClass]
+    public class SmartStackTest
+    {
+        internal class TestEntry : AbstractSmartStackElement
+        {
+            public int activationCount { get; private set; }
+            public int deactivationCount { get; private set; }
+            public int pushCount { get; private set; }
+            public int popCount { get; private set; }
+
+            public SmartStack<TestEntry> container { get { return this.Stack as SmartStack<TestEntry>; } }
+
+            public override void OnPushed (object stack) {
+                base.OnPushed(stack);
+                pushCount++;
+            }
+
+            public override void OnActivated () {
+                base.OnActivated();
+                activationCount++;
+            }
+
+            public override void OnDeactivated () {
+                deactivationCount++;
+                base.OnDeactivated();
+            }
+
+            public override void OnPopped () {
+                popCount++;
+                base.OnPopped();
+            }
+        }
+
+        [TestMethod]
+        public void TestSmartStack () {
+
+            SmartStack<TestEntry> stack = new SmartStack<TestEntry>();
+            Assert.IsTrue(stack.Empty && stack.Count == 0 && stack.Peek() == null);
+
+            // make an empty something
+            TestEntry e1 = new TestEntry();
+            Assert.IsTrue(e1.pushCount == 0 && e1.popCount == 0 && e1.activationCount == 0 && e1.deactivationCount == 0);
+            Assert.IsNull(e1.container);
+            Assert.IsFalse(e1.IsActive);
+            Assert.IsFalse(e1.IsOnStack);
+
+            // push it! make sure it's there, and got notified appropriately
+            stack.Push(e1);
+            Assert.IsTrue(stack.Peek() == e1 && stack.Count == 1 && !stack.Empty);
+            Assert.IsTrue(e1.pushCount == 1 && e1.activationCount == 1);
+            Assert.IsTrue(e1.IsOnStack);
+            Assert.IsTrue(e1.IsActive);
+
+            // push something else. this should deactivate but not pop the first one
+            TestEntry e2 = new TestEntry();
+            stack.Push(e2);
+            Assert.IsTrue(stack.Peek() == e2 && stack.Count == 2 && !stack.Empty);
+            Assert.IsTrue(e1.pushCount == 1 && e1.activationCount == 1); // same as before, but
+            Assert.IsTrue(e1.IsOnStack);
+            Assert.IsFalse(e1.IsActive); // ...this is no longer true
+            Assert.IsTrue(e1.deactivationCount == 1); // because it's been deactivated (not on top anymore)
+
+            // pop that something else, let's verify e1 is activated 
+            var popped = stack.Pop();
+            Assert.IsTrue(popped == e2);
+            Assert.IsTrue(stack.Peek() == e1 && stack.Count == 1 && !stack.Empty);
+            Assert.IsTrue(e1.pushCount == 1 && e1.activationCount == 2); // activations incremented
+            Assert.IsTrue(e1.deactivationCount == 1); // deactivations unchanged
+            Assert.IsTrue(e1.IsActive);
+
+            // now pop the last thing
+            var popped2 = stack.Pop();
+            Assert.IsTrue(popped2 == e1);
+            Assert.IsTrue(stack.Peek() == null && stack.Count == 0 && stack.Empty);
+            Assert.IsTrue(e1.pushCount == 1 && e1.activationCount == 2); // activations unchanged
+            Assert.IsTrue(e1.deactivationCount == 2 && e1.popCount == 1); // deactivations and popped incremented
+            Assert.IsFalse(e1.IsActive);     // none of these is true anymore
+            Assert.IsFalse(e1.IsOnStack);
+            Assert.IsNull(e1.container);
+        }
+    }
+}
