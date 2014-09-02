898c9d36c8819960067869f543b044367e3de76e
robert <robert>
2014-09-01 23:28:55 -0500

    Added smart queues: they tell contained objects when they're enqueued or dequeued, when they get to the front of the queue and pop off from it, and when they're being pushed back if client code adds an element to the head instead of the tail.

---
+++ Source/Collections/SmartQueue.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.Collections
+{
+    /// <summary>
+    /// Interface for queue elements that get informed when they're added or removed,
+    /// as well as start or end being the head element.
+    /// </summary>
+    public interface ISmartQueueElement
+    {
+        /// <summary>
+        /// Called after this element was enqueued onto the queue. If this is the 
+        /// only element, it will be activated as the front element after this call.
+        /// </summary>
+        void OnEnqueued (object queue);
+
+        /// <summary>
+        /// Called when this queue element becomes the head element. This could happen
+        /// because 1. another head element in front of it just got dequeued, or
+        /// 2. this element was enqueued as the only element.
+        /// </summary>
+        void OnActivated ();
+
+        /// <summary>
+        /// Called when the queue element ceases to be the head one. This happens either
+        /// as a result of 1. getting dequeued (in which case this function gets called 
+        /// before the dequeue one), or 2. another object being pushed back in front of it.
+        /// </summary>
+        void OnDeactivated (bool pushedback);
+
+        /// <summary>
+        /// Called just before this element of the queue gets dequeued. This happens
+        /// either because it's a head element that got dequeued, it because it's the last
+        /// that got popped from the tail. 
+        /// </summary>
+        void OnDequeued (bool poppedtail);
+    }
+
+    abstract public class AbstractSmartQueueElement : ISmartQueueElement
+    {
+        public object Queue { get; private set; }
+        public bool IsHead { get; private set; }
+        public bool IsEnqueued { get { return Queue != null; } }
+
+        /// <inheritDoc />
+        virtual public void OnEnqueued (object queue) {
+            this.Queue = queue;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnActivated () {
+            this.IsHead = true;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnDeactivated (bool pushedback) {
+            this.IsHead = false;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnDequeued (bool poppedtail) {
+            this.Queue = null;
+        }
+    }
+
+    [DebuggerDisplay("Count = {Count}")]
+    public class SmartQueue<T> : IEnumerable<T>, ICollection<T>, IEnumerable
+        where T : class, ISmartQueueElement
+    {
+        private LinkedList<T> _queue;
+
+        public SmartQueue () {
+            this._queue = new LinkedList<T>();
+        }
+
+        /// <summary>
+        /// Returns true if the queue is empty
+        /// </summary>
+        public bool Empty { get { return _queue.Count == 0; } }
+
+        /// <summary>
+        /// Adds a new element to the end of the queue. If it's the only element,
+        /// it will also activate it.
+        /// </summary>
+        /// <param name="element"></param>
+        public void Enqueue (T element) {
+            _queue.AddLast(element);
+            element.OnEnqueued(this);
+
+            if (_queue.Count == 1) {
+                element.OnActivated();
+            }
+        }
+
+        /// <summary>
+        /// Adds a new element to the front of the queue, automatically making it
+        /// the head of the queue, and deactivating the previous one if it existed.
+        /// </summary>
+        /// <param name="element"></param>
+        public void PushHead (T element) {
+            if (!Empty) {
+                T head = _queue.First.Value;
+                head.OnDeactivated(true);
+            }
+
+            _queue.AddFirst(element);
+            element.OnEnqueued(this);
+            element.OnActivated();
+        }
+
+        /// <summary>
+        /// Removes and returns the head element of the queue, or null if the queue is empty. 
+        /// Also activates the next element in the queue, if one exists.
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue () {
+            if (Empty) {
+                return null;
+            }
+
+            T head = _queue.First.Value;
+            head.OnDeactivated(false);
+            head.OnDequeued(false);
+            _queue.RemoveFirst();
+
+            if (!Empty) {
+                _queue.First.Value.OnActivated();
+            }
+
+            return head;
+        }
+
+        /// <summary>
+        /// Removes the last element of the queue. If it was not active (ie. not the head element),
+        /// it will not be deactivated, just dequeued. Returns null if the queue was empty.
+        /// </summary>
+        /// <returns></returns>
+        public T PopTail () {
+            if (Empty) {
+                return null;
+            }
+
+            T tail = _queue.Last.Value;
+            if (_queue.Count == 1) {
+                tail.OnDeactivated(false);
+            }
+
+            tail.OnDequeued(true);
+            _queue.RemoveLast();
+
+            return tail;
+        }
+
+        /// <summary>
+        /// Returns the head element on the queue without removing it, or null if empty.
+        /// </summary>
+        /// <returns></returns>
+        public T Head { get { return Empty ? null : _queue.First.Value; } }
+
+        /// <summary>
+        /// Returns the tail element of the queue without removing it, or null if empty.
+        /// </summary>
+        /// <returns></returns>
+        public T Tail { get { return Empty ? null : _queue.Last.Value; } }
+
+        #region IEnumerable Members
+
+        /// <inheritDoc />
+        public IEnumerator<T> GetEnumerator () {
+            return _queue.GetEnumerator();
+        }
+
+        /// <inheritDoc />
+        IEnumerator IEnumerable.GetEnumerator () {
+            return _queue.GetEnumerator();
+        }
+
+        #endregion
+
+        #region ICollection Members
+
+        /// <inheritDoc />
+        public void CopyTo (T[] array, int index) {
+            _queue.CopyTo(array, index);
+        }
+
+        /// <inheritDoc />
+        public int Count {
+            get { return _queue.Count; }
+        }
+
+        /// <inheritDoc />
+        public bool IsSynchronized {
+            get { return false; }
+        }
+
+        /// <inheritDoc />
+        public object SyncRoot {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <inheritDoc />
+        public void Add (T item) {
+            Enqueue(item);
+        }
+
+        /// <inheritDoc />
+        public void Clear () {
+            while (Count > 0) {
+                PopTail();
+            }
+        }
+
+        /// <inheritDoc />
+        public bool Contains (T item) {
+            return _queue.Contains(item);
+        }
+
+        /// <inheritDoc />
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        /// <inheritDoc />
+        public bool Remove (T item) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/Collections/SmartQueueTest.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SomaSim.Collections
+{
+    [TestClass]
+    public class SmartQueueTest
+    {
+        internal class TestEntry : AbstractSmartQueueElement
+        {
+            public int activationCount { get; private set; }
+            public int deactivationCount { get; private set; }
+            public int enqueuedCount { get; private set; }
+            public int dequeuedCount { get; private set; }
+
+            public int pushedBackCount { get; private set; }
+            public int poppedTailCount { get; private set; }
+
+            public SmartQueue<TestEntry> container { get { return this.Queue as SmartQueue<TestEntry>; } }
+
+            public override void OnEnqueued (object queue) {
+                base.OnEnqueued(queue);
+                enqueuedCount++;
+            }
+
+            public override void OnActivated () {
+                base.OnActivated();
+                activationCount++;
+            }
+
+            public override void OnDeactivated (bool pushedback) {
+                if (pushedback) { pushedBackCount++; }
+                deactivationCount++;
+                base.OnDeactivated(pushedback);
+            }
+
+            public override void OnDequeued (bool poppedtail) {
+                if (poppedtail) { poppedTailCount++; }
+                dequeuedCount++;
+                base.OnDequeued(poppedtail);
+            }
+        }
+
+        [TestMethod]
+        public void TestSmartQueueEnqueueAndDequeue () {
+
+            SmartQueue<TestEntry> q = new SmartQueue<TestEntry>();
+            Assert.IsTrue(q.Empty && q.Count == 0 && q.Head == null);
+
+            // make an empty something
+            TestEntry e1 = new TestEntry();
+            Assert.IsTrue(e1.enqueuedCount == 0 && e1.dequeuedCount == 0 && e1.activationCount == 0 && e1.deactivationCount == 0);
+            Assert.IsNull(e1.container);
+            Assert.IsFalse(e1.IsHead);
+            Assert.IsFalse(e1.IsEnqueued);
+
+            // enqueue it. make sure it's there, and got notified appropriately
+            q.Enqueue(e1);
+            Assert.IsTrue(q.Head == e1 && q.Tail == e1 && q.Count == 1 && !q.Empty);
+            Assert.IsTrue(e1.enqueuedCount == 1 && e1.activationCount == 1);
+            Assert.IsTrue(e1.IsEnqueued);
+            Assert.IsTrue(e1.IsHead);
+
+            // enqueue something else behind it. the first one should be unaffected
+            TestEntry e2 = new TestEntry();
+            q.Enqueue(e2);
+            Assert.IsTrue(q.Head == e1 && q.Tail == e2 && q.Count == 2 && !q.Empty);
+            Assert.IsTrue(e1.enqueuedCount == 1 && e1.activationCount == 1); // same as before
+            Assert.IsTrue(e1.IsEnqueued);
+            Assert.IsTrue(e1.IsHead); 
+            // but the second one shouldn't be active yet
+            Assert.IsTrue(e2.enqueuedCount == 1 && e2.activationCount == 0); // enq'd, inactive
+            Assert.IsTrue(e2.IsEnqueued);
+            Assert.IsFalse(e2.IsHead);
+
+            // dequeue e1, verify it cleaned up
+            var popped = q.Dequeue();
+            Assert.IsTrue(popped == e1);
+            Assert.IsTrue(q.Head == e2 && q.Count == 1 && !q.Empty);
+            Assert.IsTrue(e1.enqueuedCount == 1 && e1.activationCount == 1); // activations unchanged
+            Assert.IsTrue(e1.deactivationCount == 1 && e1.dequeuedCount == 1); // deactivations updated
+            Assert.IsFalse(e1.IsHead);
+            Assert.IsFalse(e1.IsEnqueued);
+            Assert.IsTrue(e1.pushedBackCount == 0 && e1.poppedTailCount == 0); // nothing unexpected here
+            // verify e2 activated
+            Assert.IsTrue(e2.enqueuedCount == 1 && e2.activationCount == 1); 
+            Assert.IsTrue(e2.IsHead);
+
+            // now deq e2 and verify things cleaned up
+            var popped2 = q.Dequeue();
+            Assert.IsTrue(popped2 == e2);
+            Assert.IsTrue(q.Head == null && q.Count == 0 && q.Empty);
+            Assert.IsTrue(e2.enqueuedCount == 1 && e2.activationCount == 1); // activations unchanged
+            Assert.IsTrue(e2.deactivationCount == 1 && e2.dequeuedCount == 1); // deactivations updated
+            Assert.IsTrue(e2.pushedBackCount == 0);
+            Assert.IsFalse(e2.IsHead);     // none of these is true anymore
+            Assert.IsFalse(e2.IsEnqueued);
+            Assert.IsNull(e2.container);
+        }
+
+        [TestMethod]
+        public void TestSmartQueuePushHead () {
+
+            SmartQueue<TestEntry> q = new SmartQueue<TestEntry>();
+            Assert.IsTrue(q.Empty && q.Count == 0 && q.Head == null);
+
+            // enqueue e1 and e2 onto the queue
+            TestEntry e1 = new TestEntry();
+            TestEntry e2 = new TestEntry();
+            q.Enqueue(e1);
+            q.Enqueue(e2);
+            Assert.IsTrue(q.Head == e1 && q.Tail == e2 && q.Count == 2);
+            Assert.IsTrue(e1.IsEnqueued);
+            Assert.IsTrue(e1.IsHead);
+
+            // now push back something in front of e1
+            TestEntry h = new TestEntry();
+            q.PushHead(h);
+            Assert.IsTrue(q.Head == h && q.Tail == e2 && q.Count == 3);
+            Assert.IsTrue(h.IsHead && h.IsEnqueued);
+            Assert.IsTrue(e1.IsEnqueued);
+            Assert.IsFalse(e1.IsHead); // no longer the head
+            Assert.IsTrue(e1.pushedBackCount == 1); // it knows it got pushed back
+
+            // now let's dequeue it and verify we're back to previous state
+            var popped = q.Dequeue();
+            Assert.IsTrue(popped == h);
+            Assert.IsFalse(h.IsHead);
+            Assert.IsFalse(h.IsEnqueued);
+            Assert.IsTrue(q.Head == e1 && q.Tail == e2 && q.Count == 2);
+            Assert.IsTrue(e1.IsHead && e1.IsEnqueued);
+            Assert.IsTrue(e1.pushedBackCount == 1);
+        }
+
+        [TestMethod]
+        public void TestSmartQueuePopTail () {
+
+            SmartQueue<TestEntry> q = new SmartQueue<TestEntry>();
+            Assert.IsTrue(q.Empty && q.Count == 0 && q.Head == null);
+
+            // enqueue e1 and e2 onto the queue
+            TestEntry e1 = new TestEntry();
+            TestEntry e2 = new TestEntry();
+            q.Enqueue(e1);
+            q.Enqueue(e2);
+            Assert.IsTrue(q.Head == e1 && q.Tail == e2 && q.Count == 2);
+            Assert.IsTrue(e1.IsEnqueued);
+            Assert.IsTrue(e1.IsHead);
+
+            // now pop e2 from the tail, make sure it never got activated or deactivated
+            var popped = q.PopTail();
+            Assert.IsTrue(q.Head == e1 && q.Tail == e1 && q.Count == 1);
+            Assert.IsFalse(e2.IsEnqueued);
+            Assert.IsFalse(e2.IsHead);
+            Assert.IsTrue(e2.enqueuedCount == 1 && e2.dequeuedCount == 1);
+            Assert.IsTrue(e2.activationCount == 0 && e2.deactivationCount == 0); // never activated or deactivated
+            Assert.IsTrue(e2.poppedTailCount == 1); // it knows it got popped from the tail
+
+            // now let's pop the head element from the tail, make sure it's cleaned up
+            var popped2 = q.PopTail();
+            Assert.IsTrue(popped2 == e1);
+            Assert.IsTrue(q.Empty);
+            Assert.IsFalse(e1.IsHead);
+            Assert.IsFalse(e1.IsEnqueued);
+            Assert.IsTrue(e1.enqueuedCount == 1 && e1.dequeuedCount == 1);
+            Assert.IsTrue(e1.activationCount == 1 && e1.deactivationCount == 1); // both activated and deactivated
+            Assert.IsTrue(e1.poppedTailCount == 1); // it also knows it got popped from the tail
+        }
+
+    }
+}
