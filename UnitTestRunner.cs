5f4cebbb56de58d78665ab8496dbe3b2a067113c
robert <robert>
2014-11-11 23:05:02 -0600

    Added a behavior that will run all methods annotated with [TestMethod], inside classes annotated with [TestClass], similarly to VStudio's unit testing harness.

---
+++ UnitTestRunner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace SomaSim
+{
+    public class UnitTestRunner : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        public void Start () {
+            System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+            RunUnitTests();
+            timer.Stop();
+            Debug.Log("Unit tests ran in " + timer.Elapsed.TotalSeconds + " seconds");
+        }
+
+        private void RunUnitTests () {
+            foreach (Type testClass in GetTestClasses()) {
+                var instance = Activator.CreateInstance(testClass);
+                RunTestMethods(instance);
+            }
+        }
+
+        private void RunTestMethods (object testInstance) {
+            foreach (MethodInfo method in testInstance.GetType().GetMethods()) {
+                if (method.GetCustomAttributes(typeof(TestMethod), true).Length > 0) {
+                    method.Invoke(testInstance, null);
+                }
+            }
+        }
+
+        private static IEnumerable<Type> GetTestClasses () {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+                foreach (Type type in assembly.GetTypes()) {
+                    if (type.GetCustomAttributes(typeof(TestClass), true).Length > 0) {
+                        yield return type;
+                    }
+                }
+            }
+        }
+    }
+#endif
+}
