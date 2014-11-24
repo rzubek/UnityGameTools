217f66472da9b39f5f187ef37ad8936e415da99a
robert <robert@somasim.com>
2014-11-24 15:07:21 -0600

    First pass at an action queue

---
+++ Source/AI/Actions.cs
@@ -0,0 +1,186 @@
+﻿using SomaSim.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.AI
+{
+    abstract public class Action : AbstractSmartQueueElement
+    {
+        internal bool _updatedOnce = false;
+
+        /// <summary>
+        /// Returns true while the action is active and also has had at least one update cycle. 
+        /// Ceases to be true when the action is deactivated or dequeued.
+        /// </summary>
+        public bool IsStarted { get { return this.IsEnqueued && this.IsActive && _updatedOnce; } }
+
+        public Script Script { get { return Queue as Script; } }
+
+        virtual internal void OnStarted () {
+            // no op
+        }
+
+        virtual internal void OnUpdate () {
+            // no op
+        }
+
+        virtual public void Stop () {
+            if (this.IsEnqueued && this.IsActive) {
+                this.Script.StopCurrentAction();
+
+            } else if (ScriptQueue.DEBUG) {
+                // this action is not running, so something went wrong if someone is calling stop()
+                throw new Exception("Can't stop action that isn't running: " + this);
+            }
+        }
+    }
+
+    public class Script : SmartQueue<Action>, ISmartQueueElement
+    {
+        public string Name;
+
+        public ScriptQueue Queue { get; private set; }
+        public bool IsActive { get; private set; }
+        public bool IsEnqueued { get { return this.Queue != null; } }
+
+        public Script (string name = null, IEnumerable<Action> actions = null) { 
+            this.Name = name;
+            if (actions != null) {
+                this.Enqueue(actions);
+            }
+        }
+
+        /// <inheritDoc />
+        virtual public void OnEnqueued (object queue) {
+            this.Queue = queue as ScriptQueue;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnActivated () {
+            this.IsActive = true;
+            if (IsEmpty) {
+                // if empty, we shouldn't be here, pop ourselves off
+                StopScript();
+            }
+        }
+
+        /// <inheritDoc />
+        virtual public void OnDeactivated (bool pushedback) {
+            // deactivate and pop all actions we contain
+            Clear();
+
+            this.IsActive = false;
+        }
+
+        /// <inheritDoc />
+        virtual public void OnDequeued (bool poppedtail) {
+            this.Queue = null;
+        }
+
+        /// <summary>
+        /// Stops the currently active action and advances to the next one or,
+        /// if that was the last action in the script, stops the script as well.
+        /// </summary>
+        public void StopCurrentAction () {
+            // remove the action
+            this.Dequeue(); 
+            // if we're out of actions, shut down
+            if (this.IsEmpty && this.IsActive) {
+                StopScript();
+            }
+        }
+
+        /// <summary>
+        /// Stops this script, which also has the effect of stopping the 
+        /// currently active action, and cancelling any other ones that have not yet activated.
+        /// </summary>
+        public void StopScript () {
+            if (this.IsEnqueued && this.IsActive) {
+                this.Queue.Dequeue();
+            } else if (ScriptQueue.DEBUG) {
+                throw new InvalidOperationException("Can't stop a script that isn't running: " + this);
+            }
+        }
+
+        /// <summary>
+        /// Called by the action queue on a regular basis. Calls update on the currently active action.
+        /// </summary>
+        internal void OnUpdate () {
+            if (this.Count == 0) {
+                if (ScriptQueue.DEBUG) {
+                    throw new InvalidOperationException("Can't update an empty script, it should have been dequened already");
+                }
+                StopScript(); // we have nothing left, bail
+                return;
+            }
+
+            // everything is okay, update 
+            Action a = this.Head;
+            if (!a._updatedOnce) {
+                a._updatedOnce = true;
+                a.OnStarted();
+            }
+            a.OnUpdate();
+        }
+
+        public override string ToString () {
+            string elements = (Queue == null || Queue.IsEmpty) ? "(none)" :
+                string.Join(", ", this.Select(action => action.ToString()).ToArray());
+            return "Script " + Name + ": " + elements;
+        }
+    }
+
+    public class ScriptQueue : SmartQueue<Script>
+    {
+        #if UNITY_EDITOR
+        public static bool DEBUG = true;
+        #else
+        public static bool DEBUG = false;
+        #endif
+
+        /// <summary>
+        /// Adds a new script, and if it's the only one, activates it immedately.
+        /// </summary>
+        /// <param name="script"></param>
+        public void Run (Script script) {
+            Enqueue(script);
+        }
+
+        /// <summary>
+        /// Adds new script, and if the queue was empty, activates the first one
+        /// immediately (before additional ones are added).
+        /// </summary>
+        /// <param name="scripts"></param>
+        public void Run (IEnumerable<Script> scripts) {
+            Enqueue(scripts);
+        }
+
+        /// <summary>
+        /// Stops and dequeues the currently active script (if any). If there was  
+        /// another script in the queue behind it, activates it.
+        /// </summary>
+        public void StopCurrentScript () {
+            Dequeue();
+        }
+
+        /// <summary>
+        /// Stops and dequeues all scripts in the queue. This deactivates the currently
+        /// active one, but does not activate or deactivate remaining ones. 
+        /// </summary>
+        public void StopAllScripts () {
+            Clear();
+        }
+
+        public void OnUpdate () {
+            if (!IsEmpty) {
+                this.Head.OnUpdate();
+            }
+        }
+
+        public override string ToString () {
+            return "ScriptQueue: " + string.Join(", ", this.Select(seq => seq.Name).ToArray());
+        }
+    }
+}
---
+++ UnitTests/AI/ActionsTest.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.AI
+{
+    [TestClass]
+    public class ActionsTest
+    {
+        public class TestAction : Action
+        {
+            public string name;
+            public int countActivate;
+            public int countDeactivate;
+
+            public TestAction (string name) {
+                this.name = name;
+            }
+
+            public override void OnActivated () {
+                base.OnActivated();
+                this.countActivate++;
+            }
+
+            public override void OnDeactivated (bool pushedback) {
+                this.countDeactivate++;
+                base.OnDeactivated(pushedback);
+            }
+
+            public override string ToString () {
+                return "TestAction " + name;
+            }
+        }
+
+        public class TestScript : Script
+        {
+            public int countActivate;
+            public int countDeactivate;
+
+            public TestScript (string name, params Action[] actions) : base(name, actions) { }
+
+            public override void OnActivated () {
+                base.OnActivated();
+                this.countActivate++;
+            }
+
+            public override void OnDeactivated (bool pushedback) {
+                this.countDeactivate++;
+                base.OnDeactivated(pushedback);
+            }
+        }
+
+        [TestMethod]
+        public void TestActionsInScript () {
+
+            // make some actions
+            var a = new TestAction("a");
+            var b = new TestAction("b");
+            var c = new TestAction("c");
+            var d = new TestAction("d");
+            var e = new TestAction("e");
+            var f = new TestAction("f");
+
+            var seq = new TestScript("test");
+            seq.Enqueue(new Action[] { a, b, c });
+
+            // make sure they're all enqueued but only A is activated
+            Assert.IsTrue(!seq.IsEmpty && seq.Head == a);
+            Assert.IsTrue(a.IsEnqueued && a.IsActive);
+            Assert.IsTrue(b.IsEnqueued && !b.IsActive);
+            Assert.IsTrue(c.IsEnqueued && !c.IsActive);
+
+            // stop A, this should dequeue it and activate B
+            a.Stop();
+            Assert.IsTrue(!seq.IsEmpty && seq.Head == b);
+            Assert.IsTrue(!a.IsEnqueued && !a.IsActive);
+            Assert.IsTrue( b.IsEnqueued &&  b.IsActive);
+            Assert.IsTrue( c.IsEnqueued && !c.IsActive);
+
+            // stop the script, this should pop B and C without activating the latter
+            seq.Clear();
+            Assert.IsTrue(seq.IsEmpty && seq.Head == null);
+            Assert.IsTrue(!a.IsEnqueued && !a.IsActive && a.countActivate == 1 && a.countDeactivate == 1);
+            Assert.IsTrue(!b.IsEnqueued && !b.IsActive && b.countActivate == 1 && b.countDeactivate == 1);
+            Assert.IsTrue(!c.IsEnqueued && !c.IsActive && c.countActivate == 0 && c.countDeactivate == 0);
+
+            // push two more 
+            seq.Enqueue(new Action[] { d, e });
+            Assert.IsTrue(d.IsEnqueued &&  d.IsActive && d.countActivate == 1 && d.countDeactivate == 0);
+            Assert.IsTrue(e.IsEnqueued && !e.IsActive && e.countActivate == 0 && e.countDeactivate == 0);
+
+            // pop E from the end, this should not affect D, or activate/deactivate E
+            seq.PopTail();
+            Assert.IsTrue( d.IsEnqueued &&  d.IsActive && d.countActivate == 1 && d.countDeactivate == 0);
+            Assert.IsTrue(!e.IsEnqueued && !e.IsActive && e.countActivate == 0 && e.countDeactivate == 0);
+
+            // push F to the front, this should deactivate D but keep it in the queue
+            seq.PushHead(f);
+            Assert.IsTrue(f.IsEnqueued &&  f.IsActive && f.countActivate == 1 && f.countDeactivate == 0);
+            Assert.IsTrue(d.IsEnqueued && !d.IsActive && d.countActivate == 1 && d.countDeactivate == 1);
+
+            // now dequeue F from the front, this should pop it, and re-activate D
+            seq.Dequeue();
+            Assert.IsTrue(!f.IsEnqueued && !f.IsActive && f.countActivate == 1 && f.countDeactivate == 1);
+            Assert.IsTrue( d.IsEnqueued &&  d.IsActive && d.countActivate == 2 && d.countDeactivate == 1);
+        }
+
+        [TestMethod]
+        public void TestActionStartupAndUpdate () {
+
+            // make some actions
+            var a = new TestAction("a");
+            var b = new TestAction("b");
+            var c = new TestAction("c");
+            var seq = new TestScript("test", a, b, c);
+
+            Assert.IsTrue(a.IsEnqueued && a.IsActive && !a.IsStarted);
+            
+            // fake an update cycle
+            seq.OnUpdate();
+
+            Assert.IsTrue(a.IsEnqueued && a.IsActive && a.IsStarted);
+
+            // stop. this will remove and deactivate A, and activate B, 
+            // but not start it yet until an update
+            a.Stop();
+            Assert.IsTrue(!a.IsEnqueued && !a.IsActive && !a.IsStarted);
+            Assert.IsTrue( b.IsEnqueued &&  b.IsActive && !b.IsStarted);
+
+            seq.OnUpdate();
+            Assert.IsTrue(b.IsEnqueued && b.IsActive && b.IsStarted);
+        }
+
+        [TestMethod]
+        public void TestScriptsInQueue () {
+            // make some scripts
+
+            var a = new TestAction("a");
+            var b = new TestAction("b");
+            var c = new TestAction("c");
+            var abc = new TestScript("abc", a, b, c);
+            var empty = new TestScript("empty");
+            var d = new TestAction("d");
+            var e = new TestAction("e");
+            var f = new TestAction("f");
+            var def = new TestScript("def", d, e, f );
+
+            var q = new ScriptQueue();
+            q.Enqueue(new Script[] { abc, empty, def });
+
+            // verify the first script and action are active 
+            Assert.IsTrue(!q.IsEmpty && q.Head == abc && q.Head.Head == a);
+            Assert.IsTrue(  abc.IsEnqueued &&  abc.IsActive);
+            Assert.IsTrue(empty.IsEnqueued && !empty.IsActive);
+            Assert.IsTrue(  def.IsEnqueued && !def.IsActive);
+
+            // stop the first action, keeps the same script active
+            a.Stop();
+            Assert.IsTrue(!q.IsEmpty && q.Head == abc && q.Head.Head == b);
+
+            // finish off actions. this will remove ABC from the queue
+            b.Stop();
+            c.Stop();
+            Assert.IsTrue(!q.IsEmpty && q.Head != abc);
+            Assert.IsTrue(abc.countActivate == 1 && abc.countDeactivate == 1);
+
+            // the Empty script was activated but then immediately removed, because it's empty
+            Assert.IsTrue(!empty.IsEnqueued && !empty.IsActive);
+            Assert.IsTrue(empty.countActivate == 1 && empty.countDeactivate == 1);
+            
+            // and let's make sure that DEF is the active one now
+            Assert.IsTrue(!q.IsEmpty && q.Head == def && q.Head.Head == d);
+            Assert.IsTrue(  !abc.IsEnqueued &&   !abc.IsActive);
+            Assert.IsTrue(!empty.IsEnqueued && !empty.IsActive);
+            Assert.IsTrue(   def.IsEnqueued &&    def.IsActive);
+            Assert.IsTrue(def.countActivate == 1 && def.countDeactivate == 0);
+
+            // then if we force-stop DEF, it should clean out of the queue completely
+            def.StopScript();
+            Assert.IsTrue(q.IsEmpty && q.Head == null);
+            Assert.IsTrue(!def.IsEnqueued && !def.IsActive);
+            Assert.IsTrue(def.countActivate == 1 && def.countDeactivate == 1);
+
+        }
+
+        [TestMethod]
+        public void TestQueueUpdate () {
+            // make some scripts
+
+            var a = new TestAction("a");
+            var b = new TestAction("b");
+            var c = new TestAction("c");
+            var abc = new TestScript("abc", a, b, c);
+            var d = new TestAction("d");
+            var e = new TestAction("e");
+            var f = new TestAction("f");
+            var def = new TestScript("def", d, e, f);
+
+            var q = new ScriptQueue();
+            q.Enqueue(new Script[] { abc, def });
+
+            Assert.IsTrue(a.IsEnqueued && a.IsActive && !a.IsStarted);
+
+            // fake a queue update
+            q.OnUpdate();
+
+            Assert.IsTrue(a.IsEnqueued && a.IsActive && a.IsStarted);
+
+            // stop the script. the first action in the next script
+            // will be activated, but won't be started until another update
+            q.StopCurrentScript();
+            Assert.IsTrue(!abc.IsEnqueued && !abc.IsActive && !a.IsActive && !a.IsStarted);
+            Assert.IsTrue(def.IsEnqueued && def.IsActive && d.IsActive && !d.IsStarted);
+
+            q.OnUpdate();
+            Assert.IsTrue(def.IsEnqueued && def.IsActive && d.IsActive && d.IsStarted);
+
+            // clear everything. no more updates
+            q.StopAllScripts();
+            Assert.IsTrue(!def.IsEnqueued && !def.IsActive && !d.IsActive && !d.IsStarted);
+            Assert.IsTrue(q.IsEmpty);
+        }
+
+    }
+}
