7b78cd75157c52680cbb1d05eba4251b3ec558e3
robert <robert@somasim.com>
2015-02-03 15:38:26 -0600

    Added a new pool for factories, and a canonical implementation of a pool or StringBuilders.

---
+++ Source/ObjectPool/FactoryPool.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.Utils
+{
+    /// <summary>
+    /// Object factory pool manages a number of factories, which can be reset and reused.
+    /// The canonical example is a StringBuilder, which can be pooled efficiently
+    /// instead of generating a new instance every time.
+    /// </summary>
+    /// <typeparam name="S">Type of objects created by the factories</typeparam>
+    /// <typeparam name="T">Type of the factory being pooled</typeparam>
+    public class FactoryPool<S, T> where T : class, new()
+    {
+        /// <summary>
+        /// Function that generates instances of IFactoryPoolElement
+        /// </summary>
+        private Func<T> _creator;
+
+        /// <summary>
+        /// Function that calls the factory to produce an item of type S,
+        /// and then resets the factory to a reusable state.
+        /// </summary>
+        private Func<T, S> _finalizer;
+
+        /// <summary>
+        /// List of all entries returned from circulation, and ready for reuse.
+        /// </summary>
+        private Stack<T> _free;
+
+        /// <summary>
+        /// Number of elements currently in circulation, that haven't been returned.
+        /// </summary>
+        private int _usedcount;
+
+        public int UsedListSize { get { return _usedcount; } }
+        public int FreeListSize { get { return _free != null ? _free.Count : -1; } }
+
+        /// <summary>
+        /// Initializes the pool with a specific creator for new factories,
+        /// and a finalizer function that will create an item from the factory,
+        /// and then reset the factory.
+        /// </summary>
+        /// <param name="creator"></param>
+        public void Initialize (Func<T> creator, Func<T,S> finalizer) {
+            _creator = creator;
+            _finalizer = finalizer;
+            _free = new Stack<T>();
+        }
+
+        /// <summary>
+        /// Releases all elements and resources held by this pool.
+        /// </summary>
+        public void Release () {
+            _free.Clear();
+            _free = null;
+            _finalizer = null;
+            _creator = null;
+        }
+
+        /// <summary>
+        /// Allocates a new factory from the pool, either by pulling from the free list, or 
+        /// by calling the generator function to generate a brand new instance.
+        /// </summary>
+        /// <returns></returns>
+        public T Allocate () {
+            T element = (_free.Count > 0) ? _free.Pop() : _creator.Invoke();
+            _usedcount++;
+            return element;
+        }
+
+        /// <summary>
+        /// Generates a new element, resets the factory and returns it to the pool.
+        /// </summary>
+        /// <param name="element"></param>
+        public S ProduceValueAndFree (T element) {
+            S result = _finalizer(element);
+            _usedcount--;
+            _free.Push(element);
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Canonical implementation of a FactoryPool that keeps a pool of StringBuilder instances for reuse.
+    /// </summary>
+    public class StringBuilderPool : FactoryPool<string, StringBuilder>
+    {
+        /// <summary>
+        /// Use this initializer to properly set up the string builder pool.
+        /// </summary>
+        public void Initialize () {
+            this.Initialize(
+                () => new StringBuilder(),
+                (StringBuilder sb) => {
+                    string result = sb.ToString();
+                    sb.Length = 0;
+                    return result;
+                });
+        }
+
+        //
+        // singleton interface
+
+        private static StringBuilderPool _instance;
+        public static StringBuilderPool Instance {
+            get {
+                if (_instance == null) { 
+                    _instance = new StringBuilderPool();
+                    _instance.Initialize();
+                }
+                return _instance;
+            }
+        }
+
+    }
+}
---
+++ UnitTests/ObjectPool/FactoryPoolTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace SomaSim.Utils
+{
+    [TestClass]
+    public class FactoryPoolTest
+    {
+        [TestMethod]
+        public void TestAllocateAndFree () {
+            var pool = new StringBuilderPool();
+            pool.Initialize();
+
+            Assert.IsTrue(pool.UsedListSize == 0 && pool.FreeListSize == 0);
+
+            // allocate some instances
+            var sb1 = pool.Allocate();
+            var sb2 = pool.Allocate();
+
+            Assert.IsTrue(pool.UsedListSize == 2 && pool.FreeListSize == 0);
+            Assert.IsTrue(sb1 != sb2);
+            Assert.IsTrue(sb1.Length == 0);
+
+            // free both and then allocate two more again - we should get both back
+
+            Assert.IsTrue(pool.ProduceValueAndFree(sb1) == "");
+            Assert.IsTrue(pool.ProduceValueAndFree(sb2) == "");
+            Assert.IsTrue(pool.UsedListSize == 0 && pool.FreeListSize == 2);
+
+            var sb3 = pool.Allocate();
+            var sb4 = pool.Allocate();
+            Assert.IsTrue(pool.UsedListSize == 2 && pool.FreeListSize == 0);
+            Assert.IsTrue(sb3 == sb2);
+            Assert.IsTrue(sb4 == sb1);
+        }
+
+        [TestMethod]
+        public void TestReset () {
+            var pool = new StringBuilderPool();
+            pool.Initialize();
+
+            // allocate a new string builder and make sure we use it
+            var sb1 = pool.Allocate();
+            Assert.IsTrue(sb1.Length == 0);
+            sb1.Append("abc");
+            Assert.IsTrue(sb1.Length == 3);
+
+            // finalize it, and make sure it got reset
+            Assert.IsTrue(pool.ProduceValueAndFree(sb1) == "abc");
+            Assert.IsTrue(sb1.Length == 0);
+
+            // now allocate again, this time since it's coming from the object pool, 
+            // we'll get back the same instance
+            var sb2 = pool.Allocate();
+            Assert.IsTrue(sb1 == sb2);
+            Assert.IsTrue(sb2.Length == 0); // and it's been reset
+        }
+
+        [TestMethod]
+        public void TestPoolRelease () {
+            var pool = new StringBuilderPool();
+            pool.Initialize();
+
+            // allocate a new instance using this custom factory, make sure the factory code got called
+            var sb1 = pool.Allocate();
+            sb1.Append("abc");
+            Assert.IsTrue(pool.UsedListSize == 1);
+            Assert.IsTrue(pool.FreeListSize == 0);
+
+            // allocate and free an instance. this will grow the pool to size one
+            Assert.IsTrue(pool.ProduceValueAndFree(pool.Allocate()) == "");
+            Assert.IsTrue(pool.UsedListSize == 1);
+            Assert.IsTrue(pool.FreeListSize == 1);
+
+            // now release the entire object pool. this will clear out the free list, but will not
+            // affect the object already in circulation, since we're not tracking them.
+            pool.Release();
+            Assert.IsTrue(sb1.Length == 3);
+            Assert.IsTrue(pool.UsedListSize == 1);
+            Assert.IsTrue(pool.FreeListSize == -1);
+        }
+    }
+}
