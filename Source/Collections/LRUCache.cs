97717f6089093e1598adcf50a070bcf99488c877
robert <robert>
2016-08-03 05:52:48 -0500

    Added an LRU cache

---
+++ Source/Collections/LRUCache.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.Collections
+{
+    /// <summary>
+    /// Implementation of a least-recently used cache.
+    /// Based on http://stackoverflow.com/questions/754233/is-it-there-any-lru-implementation-of-idictionary
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public class LRUCache<K, V>
+    {
+        private class Item
+        {
+            public K key;
+            public V value;
+        }
+
+        /// <summary>
+        /// Returns the capacity of the LRU cache. Once count meets capacity, 
+        /// adding a new item will cause the oldest (least recently accessed) item to be removed.
+        /// </summary>
+        public int Capacity { get; private set; }
+
+        /// <summary>
+        /// Returns the total number of items in the LRU cache. This number will never exceed capacity.
+        /// </summary>
+        public int Count { get { return _lruList.Count; } }
+
+        private Dictionary<K, LinkedListNode<Item>> _cacheMap;
+        private LinkedList<Item> _lruList;
+
+        /// <summary>
+        /// Creates a new LRU cache with specified max capacity. 
+        /// </summary>
+        /// <param name="capacity"></param>
+        public LRUCache (int capacity) {
+            if (capacity <= 0) throw new ArgumentOutOfRangeException("capacity", "Cache capacity must be greater than zero");
+
+            this.Capacity = capacity;
+
+            this._cacheMap = new Dictionary<K, LinkedListNode<Item>>();
+            this._lruList = new LinkedList<Item>();
+        }
+
+        /// <summary>
+        /// Returns true if this cache contains a value with the specified key.
+        /// Accessing this function does not affect the key's last access time.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool ContainsKey (K key) {
+            return _cacheMap.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Retrieves cached item from the LRU cache. If the key exists in the cache,
+        /// updates its last access time and returns the value. Otherwise 
+        /// returns a default value for type V. 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public V Get (K key) {
+            LinkedListNode<Item> node;
+            if (_cacheMap.TryGetValue(key, out node)) {
+                V value = node.Value.value;
+                _lruList.Remove(node);
+                _lruList.AddLast(node);
+                return value;
+            }
+            return default(V);
+        }
+
+        /// <summary>
+        /// Adds a new item to the cache, potentially evicting the least recently used item
+        /// if the cache is at capacity. The new item becomes the most recently used.
+        /// Attempting to add a key that already exists in the cache will throw an exception.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val"></param>
+        public void Add (K key, V val) {
+            Item cacheItem = new Item() { key = key, value = val };
+            LinkedListNode<Item> newnode = new LinkedListNode<Item>(cacheItem);
+            _cacheMap.Add(key, newnode);
+            _lruList.AddLast(newnode);
+
+            if (Count > Capacity) {
+                K oldest = _lruList.First.Value.key;
+                _lruList.RemoveFirst();
+                _cacheMap.Remove(oldest);
+            }
+        }
+
+        /// <summary>
+        /// Removes the item with the specified key from the cache and returns true, 
+        /// or returns false if this key was not in the cache.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Remove (K key) {
+            LinkedListNode<Item> node;
+            if (_cacheMap.TryGetValue(key, out node)) {
+                _lruList.Remove(node);
+                return _cacheMap.Remove(key);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Clears all elements from the cache.
+        /// </summary>
+        public void Clear () {
+            _cacheMap.Clear();
+            _lruList.Clear();
+        }
+    }
+}
---
+++ UnitTests/Collections/LRUCacheTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomaSim.Collections
+{
+    [TestClass]
+    public class LRUCacheTest
+    {
+        [TestMethod]
+        public void TestLRUCache () {
+
+            var cache = new LRUCache<string, int>(3); // make a tiny cache
+
+            Assert.IsTrue(cache.Capacity == 3 && cache.Count == 0);
+
+            Assert.IsFalse(cache.ContainsKey("one"));
+            Assert.IsTrue(cache.Get("one") == 0);
+            cache.Add("one", 1);
+            Assert.IsTrue(cache.ContainsKey("one"));
+            Assert.IsTrue(cache.Get("one") == 1);
+            Assert.IsTrue(cache.Count == 1);
+
+            Assert.IsFalse(cache.ContainsKey("two"));
+            Assert.IsTrue(cache.Get("two") == 0);
+            cache.Add("two", 2);
+            Assert.IsTrue(cache.ContainsKey("two"));
+            Assert.IsTrue(cache.Get("two") == 2);
+            Assert.IsTrue(cache.Count == 2);
+
+            Assert.IsFalse(cache.ContainsKey("three"));
+            Assert.IsTrue(cache.Get("three") == 0);
+            cache.Add("three", 3);
+            Assert.IsTrue(cache.ContainsKey("three"));
+            Assert.IsTrue(cache.Get("three") == 3);
+            Assert.IsTrue(cache.Count == 3);
+
+            // we're at capacity. if we add another element, 
+            // "one" will get evicted since it's least recently used
+
+            Assert.IsTrue(cache.Count == cache.Capacity);
+
+            cache.Add("four", 4);
+            Assert.IsTrue(cache.Get("four") == 4);
+
+            Assert.IsTrue(cache.Count == 3);
+            Assert.IsTrue(cache.ContainsKey("four"));
+            Assert.IsTrue(cache.ContainsKey("three"));
+            Assert.IsTrue(cache.ContainsKey("two"));
+            Assert.IsFalse(cache.ContainsKey("one"));
+
+            // now let's touch "two" because that's the least recently used one.
+            // by doing that, we promote "three" to be the least recently used one,
+            // and adding a new entry will then evict it.
+
+            Assert.IsTrue(cache.Get("two") == 2); // touch the oldest one
+
+            Assert.IsTrue(cache.Count == cache.Capacity);
+
+            cache.Add("five", 5);
+            Assert.IsTrue(cache.Get("five") == 5);
+
+            Assert.IsTrue(cache.Count == 3);
+            Assert.IsTrue(cache.ContainsKey("five"));
+            Assert.IsTrue(cache.ContainsKey("four"));
+            Assert.IsFalse(cache.ContainsKey("three")); // evicted as lru
+            Assert.IsTrue(cache.ContainsKey("two"));    // still there
+
+            // finally we remove one item, dropping the count.
+            // adding another item will not cause evictions
+
+            Assert.IsTrue(cache.Remove("four"));
+            Assert.IsFalse(cache.ContainsKey("four"));
+            Assert.IsTrue(cache.Get("four") == 0);
+
+            Assert.IsTrue(cache.Count == cache.Capacity - 1);
+
+            cache.Add("six", 6);
+            Assert.IsTrue(cache.Get("six") == 6);
+
+            Assert.IsTrue(cache.Count == 3);
+            Assert.IsTrue(cache.ContainsKey("six"));
+            Assert.IsTrue(cache.ContainsKey("five"));
+            Assert.IsFalse(cache.ContainsKey("four"));   // removed manually
+            Assert.IsTrue(cache.ContainsKey("two"));    // still there
+
+            // test clearing
+
+            cache.Clear();
+            Assert.IsTrue(cache.Count == 0);
+
+        }
+    }
+}
